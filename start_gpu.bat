4b9b76ba6d766555efec40fc7238e31dac9546fc
Dave <57211969+khasicrypto@users.noreply.github.com>
2019-11-01 15:36:04 +0530

    Update start_gpu.bat

--- start_gpu.bat
+++ start_gpu.bat
@@ -3,5 +3,5 @@ echo "------------------------START Miner----------------------"
 SET UV_THREADPOOL_SIZE=32
 nq-miner.exe -t cuda/opencl -a "NIMIQ_Address" -p urp.best:2053 -n "Rig_Name"
 echo "------------------------END Miner----------------------"
-echo "Please check Settings again readme file"
+echo "Please check Settings and readme file"
 pause
