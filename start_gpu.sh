9af58eefd163e4f5652a09613fb9ac6cda641c36
Dave <57211969+khasicrypto@users.noreply.github.com>
2019-11-01 15:37:32 +0530

    Update start_gpu.sh

--- start_gpu.sh
+++ start_gpu.sh
@@ -3,4 +3,4 @@
 echo "------------------------START Miner----------------------"
 ./nq-miner -t cuda -a "NQ..." -p eu.nimpool.io:8444 -n "My rig"
 echo "------------------------END Miner----------------------"
-echo "something went wrong or you exited"
+echo "Please check Settings and readme file"
